5c4b7e30593452fd714483f5871d0b6199c7fe6f
Robert Durfee <rbd@csail.mit.edu>
2021-01-29 19:14:17 -0600

    new state geojson

--- scripts/simplify.sh
+++ scripts/simplify.sh
@@ -6,5 +6,5 @@ mapshaper ../data/2018-2012_Election_Data_with_2011_Wards.geojson -simplify 10% 
 mapshaper ../data/Wisconsin_Assembly_Districts_2012.geojson -simplify 10% keep-shapes -o ../data/simplified/Wisconsin_Assembly_Districts_2012.geojson
 mapshaper ../data/Wisconsin_Senate_Districts.geojson -simplify 10% keep-shapes -o ../data/simplified/Wisconsin_Senate_Districts.geojson
 mapshaper ../data/Wisconsin_Congressional_Districts_2011.geojson -simplify 10% keep-shapes -o ../data/simplified/Wisconsin_Congressional_Districts_2011.geojson
-mapshaper ../data/Wisconsin_State_Boundary_24K.geojson -simplify 10% keep-shapes -o ../data/simplified/Wisconsin_State_Boundary_24K.geojson
+cp ../data/us-states.geojson ../data/simplified/us-states.geojson
 mapshaper ../data/County_Boundaries_24K.geojson -simplify 10% keep-shapes -o ../data/simplified/County_Boundaries_24K.geojson
