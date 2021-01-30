72fab48736fb510d0a9b76364ade05deea551423
Robert Durfee <robert@durfee.io>
2021-01-29 18:31:54 -0600

    state and county borders

--- scripts/simplify.sh
+++ scripts/simplify.sh
@@ -6,3 +6,5 @@ mapshaper ../data/2018-2012_Election_Data_with_2011_Wards.geojson -simplify 10% 
 mapshaper ../data/Wisconsin_Assembly_Districts_2012.geojson -simplify 10% keep-shapes -o ../data/simplified/Wisconsin_Assembly_Districts_2012.geojson
 mapshaper ../data/Wisconsin_Senate_Districts.geojson -simplify 10% keep-shapes -o ../data/simplified/Wisconsin_Senate_Districts.geojson
 mapshaper ../data/Wisconsin_Congressional_Districts_2011.geojson -simplify 10% keep-shapes -o ../data/simplified/Wisconsin_Congressional_Districts_2011.geojson
+mapshaper ../data/Wisconsin_State_Boundary_24K.geojson -simplify 10% keep-shapes -o ../data/simplified/Wisconsin_State_Boundary_24K.geojson
+mapshaper ../data/County_Boundaries_24K.geojson -simplify 10% keep-shapes -o ../data/simplified/County_Boundaries_24K.geojson
